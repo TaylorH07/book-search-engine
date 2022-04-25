50729e5c7479d9dc3b67acd0d466f0c305a4f2b9
taylor.holmes <taylorh2343@gmail.com>
2022-04-25 00:48:13 -0500

    adjusted mutation

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,16 +1,16 @@
-const { AuthenticationError } = require('apollo-server-express');
-const { User } = require('../models');
-const { signToken } = require('../utils/auth');
+const { AuthenticationError } = require("apollo-server-errors");
+const { User } = require("../models");
+const { signToken } = require("../utils/auth");
 
 const resolvers = {
   Query: {
     me: async (parent, args, context) => {
       if (context.user) {
-        const userData = await User.findOne({ _id: context.user._id })
-          .select('-__v -password')
+        const userData = await User.findOne({ _id: context.user._id }).select(
+          "-__v -password"
+        );
         return userData;
       }
-
       throw new AuthenticationError('User is not logged in');
     }
   },
@@ -24,7 +24,7 @@ const resolvers = {
       return { token, user };
 
     },
-    userLogin: async (parent, { email, password }) => {
+    login: async (parent, { email, password }) => {
       const user = await User.findOne({ email });
 
       if (!user) {
