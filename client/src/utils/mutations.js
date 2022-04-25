3dc26680d47082b7658ae126a811295d4deeb1fb
taylor.holmes <taylorh2343@gmail.com>
2022-04-24 22:30:46 -0500

    added mutations

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,64 @@
+import gql from "graphql-tag";
+
+export const USER_LOGIN = gql`
+    mutation userLogin($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+            token
+            user {
+                _id
+            }
+        }
+    }
+`;
+
+export const ADD_USER = gql`
+    mutation addUser($username: String!, $email: String!, $password: String!) {
+        addUser(username: $username, email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
+`;
+
+export const BOOK_SAVE = gql`
+    mutation bookSave($input: savedBook!) {
+        bookSave(input: $input) {
+            _id
+            username
+            email
+            bookCount
+            booksSaved {
+                # _id
+                bookId
+                authors
+                image
+                link
+                title
+                description
+            }
+        }
+    }
+`;
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: ID!) {
+    removeBook(bookId: $bookId) {
+      _id
+      username
+      email
+      bookCount
+      booksSaved {
+        # _id
+        bookId
+        authors
+        image
+        link
+        title
+        description
+      }
+    }
+  }
+`;
