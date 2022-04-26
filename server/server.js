a976bac143cdd60f5909b6faf3546338b4334bca
taylor.holmes <taylorh2343@gmail.com>
2022-04-26 18:17:51 -0500

    still nit working on heroku adjusted server and ran build

--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const path = require('path');
 // import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
 const { authMiddleware } = require('./utils/auth');
 
-const path = require('path');
+
 const db = require('./config/connection');
 
 // import our typeDefs and resolvers
